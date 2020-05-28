ccf28d579ea942913ec156b8fde338c0df1ac806
Debajyoti Dutta <debajyotidutta.50@gmail.com>
2020-05-28 12:11:05 +0530

    multi

--- app.py
+++ app.py
@@ -1,18 +1,20 @@
 from flask import Flask, render_template, url_for, request, session, redirect, g, jsonify
 from selenium import webdriver
 import os
-chrome_options = webdriver.ChromeOptions()
-chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument("--no-sandbox")
-driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
-driver.get('https://gadgetguys.in')
-print(driver.page_source)
+def r():
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
+    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+    driver.get('https://gadgetguys.in')
+    print(driver.page_source)
 app = Flask(__name__)
 app.secret_key='Hellothere'
 @app.route('/')
 def index():
+    r()
     return render_template('index.html')
 if __name__ == '__main__':
     app.run(debug=True)
