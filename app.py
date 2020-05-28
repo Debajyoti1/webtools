9241c8d62b943cc5b5a08142562e5c463be4e164
Debajyoti Dutta <debajyotidutta.50@gmail.com>
2020-05-28 13:00:55 +0530

    see

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 from flask import Flask, render_template, url_for, request, session, redirect, g, jsonify
 from selenium import webdriver
 import os
-def r():
+def r(a):
     chrome_options = webdriver.ChromeOptions()
     chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--no-sandbox")
     driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
-    driver.get('https://gadgetguys.in')
+    driver.get(a)
     print(driver.title)
 app = Flask(__name__)
 app.secret_key='Hellothere'
@@ -17,10 +17,10 @@ def index():
     return render_template('index.html')
 @app.route('/perform')
 def perform():
-    u=str(request.from['url'])
+    u=str(request.form['url'])
     n=int(request.form['loop'])
     for i in range(n):
-        r()
+        r(u)
     return render_template('perform.html')
 
 if __name__ == '__main__':
