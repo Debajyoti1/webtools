b8ceca08d956549defe120f278f7f1a7e017342a
Debajyoti Dutta <debajyotidutta.50@gmail.com>
2020-05-28 11:55:18 +0530

    dsf

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, url_for, request, session, redirect, g
 
 app = Flask(__name__)
 app.secret_key='Hellothere'
-
 @app.route('/')
 def index():
     return render_template('index.html')
