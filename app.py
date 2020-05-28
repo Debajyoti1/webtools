c700a0c641bea4d186f79415c4fa29037f7684b0
Debajyoti Dutta <debajyotidutta.50@gmail.com>
2020-05-28 12:00:28 +0530

    final

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
 from flask import Flask, render_template, url_for, request, session, redirect, g, jsonify
-
+from selenium import webdriver
+import os
+chrome_options = webdriver.ChromeOptions()
+chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--no-sandbox")
+driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+driver.get('https://gadgetguys.in')
+print(driver.page_source)
 app = Flask(__name__)
 app.secret_key='Hellothere'
 @app.route('/')
