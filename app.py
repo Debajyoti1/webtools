19598ca5ccd785414e175e157919489795e1770e
Debajyoti Dutta <debajyotidutta.50@gmail.com>
2020-05-28 11:49:51 +0530

    fgs

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template, url_for, request, session, redirect, g, jsonify
+
 app = Flask(__name__)
 app.secret_key='Hellothere'
+
 @app.route('/')
 def index():
     return render_template('index.html')
-
 if __name__ == '__main__':
     app.run(debug=True)
