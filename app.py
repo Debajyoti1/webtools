8fb3d74cc26d4fe93949e413722d7ff66549b71a
Debajyoti Dutta <debajyotidutta.50@gmail.com>
2020-05-28 13:51:36 +0530

    sad

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
-global driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 def r(a):
     driver.get(a)
     print(driver.title)
