bffc9c80b34f80d0bd0b89883200e9e363c69bbc
Debajyoti Dutta <debajyotidutta.50@gmail.com>
2020-06-08 12:05:02 +0530

    thread

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 from flask import Flask, render_template, url_for, request, session, redirect, g, jsonify
 from selenium import webdriver
 import os
-
+import threading
 chrome_options = webdriver.ChromeOptions()
 chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
 driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+def r1(u,n):
+    for i in range(n):
+        r(u)
 def r(a):
     driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
     driver.get(a)
@@ -32,8 +35,8 @@ def perform():
     u='https://'+u
     n=int(session.get('loop'))
     print(u+'  '+str(n))
-    for i in range(n):
-        r(u)
+    t1 = threading.Thread(target=r1, args=(u,n,))
+    t1.start()
     return render_template('perform.html')
 
 if __name__ == '__main__':
