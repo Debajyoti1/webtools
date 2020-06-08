ad44a48839af95ed4cefb5eb553e0d5f2bc84829
Debajyoti Dutta <debajyotidutta.50@gmail.com>
2020-06-08 12:33:41 +0530

    usagent

--- app.py
+++ app.py
@@ -2,16 +2,20 @@ from flask import Flask, render_template, url_for, request, session, redirect, g
 from selenium import webdriver
 import os
 import threading
+from fake_useragent import UserAgent
+ua = UserAgent()
+usrand=ua.random
 chrome_options = webdriver.ChromeOptions()
 chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
 driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
-def r1(u,n):
+def r1(u,n,usrand):
     for i in range(n):
-        r(u)
-def r(a):
+        r(u,usrand)
+def r(a,usrand):
+    chrome_options.add_argument("user-agent="+usrand)
     driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
     driver.get(a)
     print(driver.title)
@@ -31,11 +35,13 @@ def index():
     return render_template('index.html')
 @app.route('/perform',methods=['GET','POST'])
 def perform():
+    ua = UserAgent()
+    usrand=str(ua.random)
     u=str(session.get('url'))
     u='https://'+u
     n=int(session.get('loop'))
     print(u+'  '+str(n))
-    t1 = threading.Thread(target=r1, args=(u,n,))
+    t1 = threading.Thread(target=r1, args=(u,n,usrand,))
     t1.start()
     return render_template('perform.html')
 
